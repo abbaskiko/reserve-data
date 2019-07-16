13fb494a429f8f039d36ffb9c3be19677926db04
Diep Pham <me@favadi.com>
2019-07-16 15:47:15 +0700

    remove unused configuration

--- http/http_test.go
+++ http/http_test.go
@@ -1,15 +1,10 @@
 package http
 
 import (
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
-
-	"reflect"
-
-	"github.com/KyberNetwork/reserve-data/common"
 )
 
 type assertFn func(t *testing.T, resp *httptest.ResponseRecorder)
@@ -20,45 +15,6 @@ type testCase struct {
 	method   string
 	data     map[string]string
 	assert   assertFn
-}
-
-func newAssertGetEquation(expectedData []byte) assertFn {
-	return func(t *testing.T, resp *httptest.ResponseRecorder) {
-		t.Helper()
-
-		var expected common.PWIEquationRequestV2
-		if resp.Code != http.StatusOK {
-			t.Fatalf("wrong return code, expected: %d, got: %d", http.StatusOK, resp.Code)
-		}
-
-		type responseBody struct {
-			Success bool
-			Data    common.PWIEquationRequestV2
-		}
-
-		decoded := responseBody{}
-		if aErr := json.NewDecoder(resp.Body).Decode(&decoded); aErr != nil {
-			t.Fatal(aErr)
-		}
-
-		if decoded.Success != true {
-			t.Errorf("wrong success status, expected: %t, got: %t", true, decoded.Success)
-		}
-
-		t.Logf("returned pending PWI equation request: %v", decoded.Data)
-
-		if len(decoded.Data) != 2 {
-			t.Fatalf("wrong number of tokens, expected: %d, got %d", 2, len(decoded.Data))
-		}
-
-		if aErr := json.Unmarshal(expectedData, &expected); aErr != nil {
-			t.Fatal(aErr)
-		}
-
-		if !reflect.DeepEqual(expected, decoded.Data) {
-			t.Logf("expected data doesn't match: %v, decoded: %v", expected, decoded)
-		}
-	}
 }
 
 func testHTTPRequest(t *testing.T, tc testCase, handler http.Handler) {
