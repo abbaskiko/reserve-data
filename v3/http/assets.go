c5353c099231e4d3aa23d1e6b643d98632bbb406
GiangTran <secmask@gmail.com>
2019-07-23 10:07:24 +0700

    move create asset to its own file

--- v3/http/assets.go
+++ v3/http/assets.go
@@ -35,66 +35,6 @@ func (s *Server) getAssets(c *gin.Context) {
 	httputil.ResponseSuccess(c, httputil.WithData(assets))
 }
 
-func (s *Server) createCreateAsset(c *gin.Context) {
-	var createAsset common.CreateCreateAsset
-
-	err := c.ShouldBindJSON(&createAsset)
-
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-
-	id, err := s.storage.CreateCreateAsset(createAsset)
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c, httputil.WithField("id", id))
-}
-
-func (s *Server) getCreateAssets(c *gin.Context) {
-	result, err := s.storage.GetCreateAssets()
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c, httputil.WithData(result))
-}
-
-func (s *Server) confirmCreateAsset(c *gin.Context) {
-	var input struct {
-		ID uint64 `uri:"id" binding:"required"`
-	}
-	if err := c.ShouldBindUri(&input); err != nil {
-		log.Println(err)
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	err := s.storage.ConfirmCreateAsset(input.ID)
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c)
-}
-
-func (s *Server) rejectCreateAsset(c *gin.Context) {
-	var input struct {
-		ID uint64 `uri:"id" binding:"required"`
-	}
-	if err := c.ShouldBindUri(&input); err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	err := s.storage.RejectCreateAsset(input.ID)
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c)
-}
-
 func (s *Server) createAssetExchange(c *gin.Context) {
 
 	var r common.CreateAssetExchange
