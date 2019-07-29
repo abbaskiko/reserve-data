22931bc3c7fd060124a43771ded41d899bd754c5
ducquangkstn <ducquang.kstn@gmail.com>
2019-07-29 14:47:09 +0700

    valid-update-asset-params

--- v3/http/asset_update.go
+++ v3/http/asset_update.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 
 	"github.com/KyberNetwork/reserve-data/http/httputil"
 	"github.com/KyberNetwork/reserve-data/v3/common"
@@ -18,13 +19,56 @@ func (s *Server) createUpdateAsset(c *gin.Context) {
 		httputil.ResponseFailure(c, httputil.WithError(err))
 		return
 	}
-	// TODO validate if the update request satisfy constraint
+
+	for _, updateEntry := range updateAsset.Assets {
+		if err = s.checkUpdateAssetParams(updateEntry); err != nil {
+			httputil.ResponseFailure(c, httputil.WithError(err))
+			return
+		}
+	}
+
 	id, err := s.storage.CreateUpdateAsset(updateAsset)
 	if err != nil {
 		httputil.ResponseFailure(c, httputil.WithError(err))
 		return
 	}
 	httputil.ResponseSuccess(c, httputil.WithField("id", id))
+}
+
+func (s *Server) checkUpdateAssetParams(updateEntry common.UpdateAssetEntry) error {
+	access, err := s.storage.GetAsset(updateEntry.AssetID)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get asset id: %v from db ", updateEntry.AssetID)
+	}
+
+	if updateEntry.Rebalance != nil {
+		if *updateEntry.Rebalance {
+			if access.RebalanceQuadratic == nil && updateEntry.RebalanceQuadratic == nil {
+				return common.ErrRebalanceQuadraticMissing
+			}
+
+			if access.Target == nil && updateEntry.Target == nil {
+				return common.ErrAssetTargetMissing
+			}
+		}
+	}
+
+	if updateEntry.SetRate != nil {
+		if *updateEntry.SetRate != common.SetRateNotSet && access.PWI == nil && updateEntry.PWI == nil {
+			return common.ErrPWIMissing
+		}
+	}
+
+	if updateEntry.Transferable != nil {
+		if *updateEntry.Transferable {
+			for _, exchange := range access.Exchanges {
+				if common.IsZeroAddress(exchange.DepositAddress) {
+					return common.ErrDepositAddressMissing
+				}
+			}
+		}
+	}
+	return nil
 }
 
 func (s *Server) getUpdateAssets(c *gin.Context) {
