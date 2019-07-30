bd0458f7f06a0ced42ea3a395beea23704e500e1
GiangTran <secmask@gmail.com>
2019-07-30 16:36:37 +0700

    add http test for create/update trading-pair

---
+++ v3/http/trading_pair.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/KyberNetwork/reserve-data/http/httputil"
+)
+
+func (s *Server) getTradingPair(c *gin.Context) {
+	var input struct {
+		ID uint64 `uri:"id" binding:"required"`
+	}
+	if err := c.ShouldBindUri(&input); err != nil {
+		httputil.ResponseFailure(c, httputil.WithError(err))
+		return
+	}
+	result, err := s.storage.GetTradingPair(input.ID)
+	if err != nil {
+		httputil.ResponseFailure(c, httputil.WithError(err))
+		return
+	}
+	httputil.ResponseSuccess(c, httputil.WithData(result))
+}
