2de5d11487a05fae353ac00fc8b0b576171a1f97
Hoàng Hà <halink0803@gmail.com>
2019-07-31 11:57:02 +0700

    update permission policy

--- gateway/http/permission.go
+++ gateway/http/permission.go
@@ -30,15 +30,25 @@ e = some(where (p.eft == allow))
 m = g(r.sub, p.sub)  && keyMatch(r.obj, p.obj) && regexMatch(r.act, p.act)
 `
 	)
+
 	pol := fmt.Sprintf(`
 p, %[1]s, /*, GET
 
 p, %[2]s, /*, GET
 p, %[2]s, /create-asset, POST
+p, %[2]s, /update-asset, POST 
+p, %[2]s, /create-asset-exchange, POST
+p, %[2]s, /update-exchange, POST
+p, %[2]s, /create-trading-pair, POST
+p, %[2]s, /update-trading-pair, POST
 
 p, %[3]s, /*, GET
-p, %[3]s, /create-asset/:id, PUT
-p, %[3]s, /create-asset/:id, DELETE
+p, %[3]s, /create-asset/:id, (PUT)|(DELETE)
+p, %[3]s, /update-asset/:id, (PUT)|(DELETE)
+p, %[3]s, /create-asset-exchange/:id, (PUT)|(DELETE)
+p, %[3]s, /update-exchange/:id, (PUT)|(DELETE)
+p, %[3]s, /create-trading-pair/:id, (PUT)|(DELETE) 
+p, %[3]s, /update-trading-pair/:id, (PUT)|(DELETE) 
 
 p, %[4]s, /*, GET
 `, readKeyID, writeKeyID, confirmKeyID, rebalanceKeyID)
