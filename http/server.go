4924335a1c21208ff27078f3f8e85c856bdd24ae
Hoàng Hà <halink0803@gmail.com>
2019-08-07 18:15:34 +0700

    remove unused docs

--- http/server.go
+++ http/server.go
@@ -17,7 +17,6 @@ import (
 	"github.com/KyberNetwork/reserve-data/common"
 	"github.com/KyberNetwork/reserve-data/http/httputil"
 	"github.com/KyberNetwork/reserve-data/pricefactor"
-	v3common "github.com/KyberNetwork/reserve-data/v3/common"
 	v3http "github.com/KyberNetwork/reserve-data/v3/http"
 	"github.com/KyberNetwork/reserve-data/v3/storage"
 )
@@ -323,68 +322,6 @@ func (s *Server) ValidateTimeInput(c *gin.Context) (uint64, uint64, bool) {
 		toTime = common.GetTimepoint()
 	}
 	return fromTime, toTime, true
-}
-
-// SetStableTokenParams set stable token params
-func (s *Server) SetStableTokenParams(c *gin.Context) {
-	postForm := c.Request.Form
-	value := []byte(postForm.Get("value"))
-	if len(value) > maxDataSize {
-		httputil.ResponseFailure(c, httputil.WithReason(errDataSizeExceed.Error()))
-		return
-	}
-	err := s.priceFactorStorage.SetStableTokenParams(value)
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c)
-}
-
-// ConfirmStableTokenParams confirm stable token params
-func (s *Server) ConfirmStableTokenParams(c *gin.Context) {
-	postForm := c.Request.Form
-	value := []byte(postForm.Get("value"))
-	if len(value) > maxDataSize {
-		httputil.ResponseFailure(c, httputil.WithReason(errDataSizeExceed.Error()))
-		return
-	}
-	err := s.priceFactorStorage.ConfirmStableTokenParams(value)
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c)
-}
-
-// RejectStableTokenParams reject stable token params
-func (s *Server) RejectStableTokenParams(c *gin.Context) {
-	err := s.priceFactorStorage.RemovePendingStableTokenParams()
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c)
-}
-
-// GetPendingStableTokenParams return pending stable token params
-func (s *Server) GetPendingStableTokenParams(c *gin.Context) {
-	data, err := s.priceFactorStorage.GetPendingStableTokenParams()
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c, httputil.WithData(data))
-}
-
-// GetStableTokenParams return all stable token params
-func (s *Server) GetStableTokenParams(c *gin.Context) {
-	data, err := s.priceFactorStorage.GetStableTokenParams()
-	if err != nil {
-		httputil.ResponseFailure(c, httputil.WithError(err))
-		return
-	}
-	httputil.ResponseSuccess(c, httputil.WithData(data))
 }
 
 func (s *Server) register() {
