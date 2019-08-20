31d1dab05e12809c7b29060fea1b28477c59626b
Hoàng Hà <halink0803@gmail.com>
2019-08-20 14:14:40 +0700

    update test

--- v3/http/asset_exchange_test.go
+++ v3/http/asset_exchange_test.go
@@ -118,6 +118,16 @@ func TestHTTPServer_DeleteAssetExchange(t *testing.T) {
 		settingChangePath = "/v3/setting-change"
 	)
 
+	var (
+		supportedExchanges = make(map[v1common.ExchangeID]v1common.LiveExchange)
+	)
+
+	//create map of test exchange
+	for _, exchangeID := range []v1common.ExchangeID{v1common.Binance, v1common.Huobi, v1common.StableExchange} {
+		exchange := v1common.TestExchange{}
+		supportedExchanges[exchangeID] = exchange
+	}
+
 	db, tearDown := testutil.MustNewDevelopmentDB()
 	defer func() {
 		assert.NoError(t, tearDown())
@@ -129,7 +139,7 @@ func TestHTTPServer_DeleteAssetExchange(t *testing.T) {
 	assetID, err := createSampleAsset(s)
 	require.NoError(t, err)
 
-	server := NewServer(s, "", nil)
+	server := NewServer(s, "", supportedExchanges)
 
 	//create map of test exchange
 	for _, exchangeID := range []v1common.ExchangeID{v1common.Binance, v1common.Huobi, v1common.StableExchange} {
