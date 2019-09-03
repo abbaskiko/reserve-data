2c31251f5e1f5541c30f26e32068776ce180a7e6
ducquangkstn <ducquang.kstn@gmail.com>
2019-09-03 16:10:05 +0700

    refactor deposit address v3

--- reservesetting/storage/postgres/db_types.go
+++ reservesetting/storage/postgres/db_types.go
@@ -5,7 +5,7 @@ import (
 
 	ethereum "github.com/ethereum/go-ethereum/common"
 
-	"github.com/KyberNetwork/reserve-data/v3/common"
+	"github.com/KyberNetwork/reserve-data/reservesetting/common"
 )
 
 // assetExchangeCondition is placeholder for namedStmt to select asset exchange
