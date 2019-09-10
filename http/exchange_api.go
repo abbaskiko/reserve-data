7ccdc00d8e020c1e01a07a71c5a8a475a40ca576
Hoàng Hà <halink0803@gmail.com>
2019-09-10 17:37:57 +0700

    add cancel all order api

---
+++ http/exchange_api.go
@@ -0,0 +1,30 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/KyberNetwork/reserve-data/common"
+	"github.com/KyberNetwork/reserve-data/http/httputil"
+)
+
+// CancelAllOrders cancel all orders
+func (s *Server) CancelAllOrders(c *gin.Context) {
+	pendingActivites, err := s.app.GetPendingActivities()
+	if err != nil {
+		httputil.ResponseFailure(c, httputil.WithError(err))
+		return
+	}
+
+	for _, activity := range pendingActivites {
+		if activity.Action == common.ActionTrade {
+			exchange := activity.Params["exchange"].(common.Exchange)
+			// Cancel order
+			if err := s.core.CancelOrder(activity.ID, exchange); err != nil {
+				httputil.ResponseFailure(c, httputil.WithError(err))
+				return
+			}
+		}
+	}
+
+	httputil.ResponseSuccess(c)
+}
