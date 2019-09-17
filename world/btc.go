dc78a9ec40fa9ab6ba1eeabd6fa55f34cf1f7243
GiangTran <secmask@gmail.com>
2019-09-17 16:10:18 +0700

    clean up world package

--- world/btc.go
+++ world/btc.go
@@ -1,104 +1,34 @@
 package world
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/KyberNetwork/reserve-data/common"
 )
 
 func (tw *TheWorld) getCoinbaseInfo() common.CoinbaseData {
 	var (
-		client = &http.Client{Timeout: 30 * time.Second}
 		url    = tw.endpoint.CoinbaseBTCEndpoint()
-		result = common.CoinbaseData{}
+		result = common.CoinbaseData{
+			Valid: true,
+		}
 	)
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return common.CoinbaseData{
-			Valid: false,
-			Error: err.Error(),
-		}
+	if err := tw.getPublic(url, &result); err != nil {
+		result.Valid = false
+		result.Error = err.Error()
 	}
-
-	req.Header.Add("Accept", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		return common.CoinbaseData{
-			Valid: false,
-			Error: err.Error(),
-		}
-	}
-	defer func() {
-		if cErr := resp.Body.Close(); cErr != nil {
-			log.Printf("failed to close response body: %s", cErr.Error())
-		}
-	}()
-
-	if resp.StatusCode != http.StatusOK {
-		return common.CoinbaseData{
-			Valid: false,
-			Error: fmt.Sprintf("unexpected return code: %d", resp.StatusCode),
-		}
-	}
-
-	if err = json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return common.CoinbaseData{
-			Valid: false,
-			Error: err.Error(),
-		}
-	}
-	result.Valid = true
 	return result
 }
 
 func (tw *TheWorld) getGeminiInfo() common.GeminiData {
 	var (
-		client = &http.Client{Timeout: 30 * time.Second}
 		url    = tw.endpoint.GeminiBTCEndpoint()
-		result = common.GeminiData{}
+		result = common.GeminiData{
+			Valid: true,
+		}
 	)
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return common.GeminiData{
-			Valid: false,
-			Error: err.Error(),
-		}
+	if err := tw.getPublic(url, &result); err != nil {
+		result.Valid = false
+		result.Error = err.Error()
 	}
-
-	req.Header.Add("Accept", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		return common.GeminiData{
-			Valid: false,
-			Error: err.Error(),
-		}
-	}
-	defer func() {
-		if cErr := resp.Body.Close(); cErr != nil {
-			log.Printf("failed to close response body: %s", cErr.Error())
-		}
-	}()
-
-	if resp.StatusCode != http.StatusOK {
-		return common.GeminiData{
-			Valid: false,
-			Error: fmt.Sprintf("unexpected return code: %d", resp.StatusCode),
-		}
-	}
-
-	if err = json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return common.GeminiData{
-			Valid: false,
-			Error: err.Error(),
-		}
-	}
-	result.Valid = true
 	return result
 }
 
