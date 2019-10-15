bf1df9373739117e70126ff94cbbe9f613b468d1
GiangTran <secmask@gmail.com>
2019-10-15 10:05:57 +0700

    add missing vendor file

--- vendor.sh
+++ vendor.sh
@@ -13,3 +13,12 @@ popd
 
 cp -R "$src_dir"/go-ethereum/crypto/secp256k1/libsecp256k1 \
    ./vendor/github.com/ethereum/go-ethereum/crypto/secp256k1/
+
+readonly src_usb=/tmp/reserve_data/vendor/usb
+mkdir -p "$src_usb"
+pushd "$src_usb"
+[[ -d usb ]] || git clone https://github.com/karalabe/usb.git
+popd
+
+cp -R "$src_usb"/usb/hidapi ./vendor/github.com/karalabe/usb/hidapi
+cp -R "$src_usb"/usb/libusb ./vendor/github.com/karalabe/usb/libusb
