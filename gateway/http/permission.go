12cd1e2d387a612cc8e8725cbafcdfa30ac4fc60
Hoàng Hà <halink0803@gmail.com>
2020-02-24 11:05:39 +0700

    fix update-exchange-status permission

--- gateway/http/permission.go
+++ gateway/http/permission.go
@@ -26,7 +26,7 @@ p, %[1]s, /v3/setting-change-rbquadratic, POST
 p, %[1]s, /v3/setting-change-main, POST
 p, %[1]s, /v3/setting-change-stable, POST
 p, %[1]s, /v3/setting-change-feed-configuration, POST
-p, %[1]s, /v3/update-exchange-status/:id, POST`, key)
+p, %[1]s, /v3/update-exchange-status/:id, PUT`, key)
 }
 
 func addKeyConfirmPolicy(key string) string {
