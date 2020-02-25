cc72b92da1823c0cf0e2c19fc56b690cd4d2bd19
GiangTran <secmask@gmail.com>
2020-02-25 17:44:10 +0700

    add pricing, reserve contract address to /addresses, remove unuse deployment type

--- cmd/deployment/deployment.go
+++ cmd/deployment/deployment.go
@@ -18,16 +18,8 @@ const (
 	Development Deployment = iota // develop
 	// Production is the production deployment of Kyber Network contracts on mainnet.
 	Production // production
-	// Staging is the staging deployment of Kyber Network contracts on mainnet.
-	Staging // staging
-	// Kovan is the development deployment of Kyber Network contracts on Kovan testnet.
-	Kovan // kovan
-	// DeploymentKovan is the development deployment of Kyber Network contracts on Ropsten testnet.
-	Ropsten // ropsten
 	// Simulation is the special deployment of Kyber Network contracts that supports simulation testing.
 	Simulation // simulation
-	// Analytic is the special deployment for use in analytic development.
-	Analytic // analytic
 )
 
 // NewCliFlag returns new cli flag from context.
@@ -48,16 +40,8 @@ func NewDeploymentFromContext(c *cli.Context) (Deployment, error) {
 		return Development, nil
 	case Production.String():
 		return Production, nil
-	case Staging.String():
-		return Staging, nil
-	case Kovan.String():
-		return Kovan, nil
-	case Ropsten.String():
-		return Ropsten, nil
 	case Simulation.String():
 		return Simulation, nil
-	case Analytic.String():
-		return Analytic, nil
 	}
 
 	return 0, fmt.Errorf("unknown deployment %s", deploymentStr)
