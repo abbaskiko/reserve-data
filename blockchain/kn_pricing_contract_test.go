f37923bb978d7c443f5f81d343fc8f271e7c67df
GiangTran <secmask@gmail.com>
2020-03-18 15:55:02 +0700

    fix missing param call to NewBlockChain

--- blockchain/kn_pricing_contract_test.go
+++ blockchain/kn_pricing_contract_test.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/KyberNetwork/reserve-data/common"
 	baseblockchain "github.com/KyberNetwork/reserve-data/common/blockchain"
+	"github.com/KyberNetwork/reserve-data/common/config"
 	"github.com/KyberNetwork/reserve-data/settings"
 	settingsstorage "github.com/KyberNetwork/reserve-data/settings/storage"
 )
@@ -86,7 +87,7 @@ func TestGeneratedGetListedTokens(t *testing.T) {
 		}
 	}()
 
-	blockchain, err := NewBlockchain(baseBlockchain, setting)
+	blockchain, err := NewBlockchain(baseBlockchain, setting, config.GasConfig{})
 	require.NoError(t, err)
 
 	opts := blockchain.GetCallOpts(0)
