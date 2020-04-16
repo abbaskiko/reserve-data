1c50da2d003434cd2d6a92b6c5644a6bd808ada8
GiangTran <secmask@gmail.com>
2020-04-16 16:03:04 +0700

    abort if error is not 'order not exist'

--- exchange/binance.go
+++ exchange/binance.go
@@ -261,9 +261,11 @@ func (bn *Binance) Trade(tradeType string, base common.Token, quote common.Token
 			break
 		}
 		bn.l.Errorw("failed to query order info", "i", i, "orderID", result.OrderID, "base", base.ID, "quote", quote.ID)
-		if strings.Contains(err.Error(), "Order does not exist") {
+		if strings.Contains(err.Error(), "Order does not exist") { // only retry if got specified error
 			time.Sleep(time.Second)
+			continue
 		}
+		break
 	}
 	id = strconv.FormatUint(result.OrderID, 10)
 	return id, done, remaining, finished, err
