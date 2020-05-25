6e71a505ad5763eb721ae4d59f5b9393967ad6e1
NNagato <darkdragon236@gmail.com>
2020-05-25 17:28:03 +0700

    remove unused struct data

--- common/usd_data.go
+++ common/usd_data.go
@@ -1,32 +1,5 @@
 package common
 
-// BinanceData data return by https://api.binance.com/api/v3/ticker/bookTicker?symbol=ETHUSDC
-type BinanceData struct {
-	Valid    bool
-	Error    string
-	Symbol   string `json:"symbol"`
-	BidPrice string `json:"bidPrice"`
-	BidQty   string `json:"bidQty"`
-	AskPrice string `json:"askPrice"`
-	AskQty   string `json:"askQty"`
-}
-
-// HitData ...
-type HitData struct {
-	Valid       bool
-	Error       string
-	Ask         string `json:"ask"`
-	Bid         string `json:"bid"`
-	Last        string `json:"last"`
-	Open        string `json:"open"`
-	Low         string `json:"low"`
-	High        string `json:"high"`
-	Volume      string `json:"volume"`
-	VolumeQuote string `json:"volumeQuote"`
-	Timestamp   string `json:"timestamp"`
-	Symbol      string `json:"symbol"`
-}
-
 // USDData ...
 type USDData struct {
 	Timestamp             uint64
