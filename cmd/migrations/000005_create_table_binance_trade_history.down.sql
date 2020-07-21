a21352fc5cf399e1b00d66b9752a69c29e8f3fcb
Ha Link <halink0803@gmail.com>
2020-07-21 16:36:20 +0700

    Update migration files and docker build file (#1001)
    * update migration files and docker build file
    
    * update docker build

--- cmd/migrations/000005_create_table_binance_trade_history.down.sql
+++ cmd/migrations/000005_create_table_binance_trade_history.down.sql
@@ -1,1 +1,4 @@
-DROP TABLE "binance_trade_history";
+		ALTER TABLE binance_trade_history 
+			DROP CONSTRAINT IF EXISTS binance_trade_history_pair_fkey;
+        ALTER TABLE binance_trade_history 
+			ALTER COLUMN pair_id TYPE BIGINT;
