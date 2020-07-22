a82792246ae49bc989098dc89ecd780fede5dc44
Hoàng Hà <halink0803@gmail.com>
2020-07-22 17:05:53 +0700

    update docker file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 set -euo pipefail
 
 cp /setting/* /go/src/github.com/KyberNetwork/reserve-data/cmd/
+cp -R /migrations /go/src/github.com/KyberNetwork/reserve-data/cmd/
 cd /go/src/github.com/KyberNetwork/reserve-data/cmd/
 /cmd "$@"
