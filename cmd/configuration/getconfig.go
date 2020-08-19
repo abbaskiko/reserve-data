9be0a259348541e4e7e41b75daf955bed4034ffa
NNagato <darkdragon236@gmail.com>
2020-08-19 16:47:05 +0700

    make huobi deposit 2nd tx use gasstation v3

--- cmd/configuration/getconfig.go
+++ cmd/configuration/getconfig.go
@@ -2,13 +2,14 @@ package configuration
 
 import (
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/urfave/cli"
 	"go.uber.org/zap"
 
 	"github.com/KyberNetwork/reserve-data/common"
 	"github.com/KyberNetwork/reserve-data/common/archive"
 	"github.com/KyberNetwork/reserve-data/common/blockchain"
+	"github.com/KyberNetwork/reserve-data/common/gasstation"
+	"github.com/KyberNetwork/reserve-data/core"
 	"github.com/KyberNetwork/reserve-data/exchange/binance"
 	"github.com/KyberNetwork/reserve-data/exchange/huobi"
 	"github.com/KyberNetwork/reserve-data/reservesetting/storage"
@@ -24,42 +25,24 @@ func GetConfig(
 	contractAddressConf *common.ContractAddressConfiguration,
 	settingStorage storage.Interface,
 	rcf common.RawConfig,
+	mainNode *common.EthClient, backupNodes []*common.EthClient,
+	gasClient *gasstation.Client, gasLimiter core.GasPriceLimiter,
 ) (*Config, error) {
 	l := zap.S()
 
-	//set client & endpoint
-	client, err := rpc.Dial(nodeConf.Main)
-	if err != nil {
-		return nil, err
-	}
-
-	mainClient := ethclient.NewClient(client)
 	bkClients := map[string]*ethclient.Client{}
-
-	var callClients []*common.EthClient
+	callClients := make([]*common.EthClient, 0, len(backupNodes)+1)
 
 	// add main node to callclients
-	callClients = append(callClients, &common.EthClient{
-		Client: mainClient,
-		URL:    nodeConf.Main,
-	})
-	bkClients[nodeConf.Main] = mainClient
-	for _, ep := range nodeConf.Backup {
-		var bkClient *ethclient.Client
-		bkClient, err = ethclient.Dial(ep)
-		if err != nil {
-			l.Warnw("Cannot connect to rpc endpoint", "endpoint", ep, "err", err)
-		} else {
-			bkClients[ep] = bkClient
-			callClients = append(callClients, &common.EthClient{
-				Client: bkClient,
-				URL:    ep,
-			})
-		}
+	callClients = append(callClients, mainNode)
+	bkClients[mainNode.URL] = mainNode.Client
+	for _, bn := range backupNodes {
+		callClients = append(callClients, bn)
+		bkClients[bn.URL] = bn.Client
 	}
 
 	bc := blockchain.NewBaseBlockchain(
-		client, mainClient, map[string]*blockchain.Operator{},
+		mainNode.RPCClient, mainNode.Client, map[string]*blockchain.Operator{},
 		blockchain.NewBroadcaster(bkClients),
 		blockchain.NewContractCaller(callClients),
 	)
@@ -75,10 +58,12 @@ func GetConfig(
 		World:                   theWorld,
 		ContractAddresses:       contractAddressConf,
 		SettingStorage:          settingStorage,
+		gasClient:               gasClient,
+		gasLimiter:              gasLimiter,
 	}
 
 	l.Infow("configured endpoint", "endpoint", config.EthereumEndpoint, "backup", config.BackupEthereumEndpoints)
-	if err = config.AddCoreConfig(cliCtx, rcf, bi, hi, settingStorage); err != nil {
+	if err := config.AddCoreConfig(cliCtx, rcf, bi, hi, settingStorage); err != nil {
 		return nil, err
 	}
 	return config, nil
